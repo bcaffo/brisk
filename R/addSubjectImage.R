f6d41abc028e3c0980e99a30fefad022b2e1c424
Brian Caffo <bcaffo@gmail.com>
2012-06-20 15:13:35 -0400

    Initial import

---
+++ R/addSubjectImage.R
@@ -0,0 +1,14 @@
+addSubjectImage <- function(
+  imageDataList, 
+  id, 
+  filename, 
+  directory, 
+  image.type, 
+  registered = TRUE, 
+  template = NULL, 
+  type = "nifti", 
+  verbose = TRUE
+){
+  if (type != "nifti") stop("Only nifti supported at this time")
+}
+
